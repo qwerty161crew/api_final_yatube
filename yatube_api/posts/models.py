64e1aeb373784052fb69e0f20c58dc92178ef17b
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-28 13:29:30 +0300

    в модель пост добавил поле группа и сделал миграцию

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,7 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
@@ -13,13 +12,18 @@ class Group(models.Model):
     def __str__(self):
         return self.title
 
+
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        'Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL,
+        related_name='posts', blank=True, null=True, verbose_name='группа')
 
     def __str__(self):
         return self.text
