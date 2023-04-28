5373aaa8ccabd96f033333597feb73d12487790c
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-28 16:27:52 +0300

    поменял пермишн в вью группы. 37 тестов

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -22,7 +22,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (GetNotAuchOnly, )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
