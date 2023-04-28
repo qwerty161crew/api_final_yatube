3921c1e93253074b610e57b232b89736eab819e7
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-28 13:17:50 +0300

    добавлен урл для подписки и урл для получения токена

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,19 +2,23 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from api.views import PostViewSet, GroupViewSet, CommentViewSet
+from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowingViewSet
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='post')
 router.register('groups', GroupViewSet, basename='group')
 router.register(r'posts/(?P<post_id>\d+)/comments',
                 CommentViewSet, basename='comment')
+router.register('follow', FollowingViewSet, basename='follow')
+
 
 
 app_name = 'api'
 
 urlpatterns = [
-    path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/', include((router.urls, 'api'))),
+    path('v1/auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('v1/auth/', include('djoser.urls.jwt')),
 
 ]
