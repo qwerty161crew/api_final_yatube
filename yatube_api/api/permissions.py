9423af06544d755cccee87c08b4f7b6a4f3e60ab
Алексей Матвеев <qwerty161crew@gmail.com>
2023-04-28 13:16:54 +0300

    переопределение пермишена

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,7 +1,18 @@
 from rest_framework import permissions
 
 
-class AuthorCreateorDeleteOnly(permissions.BasePermission):
+class AuthorDeleteOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method not in permissions.SAFE_METHODS:
+            if obj.author == request.user:
+                return True
+            return False
+
+        return True
+
+
+class AuchCreateOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
